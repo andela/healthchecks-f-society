50501f317e133d9351d13dd85a2143efd607b280
devGenie <jonen54@gmail.com>
2017-09-05 14:25:16 +0300

    [Feature #150704022] Test webhook handles internal error 500

--- hc/api/tests/test_notify.py
+++ hc/api/tests/test_notify.py
@@ -222,4 +222,14 @@ class NotifyTestCase(BaseTestCase):
         json = kwargs["json"]
         self.assertEqual(json["message_type"], "CRITICAL")
 
-    ### Test that the web hooks handle connection errors and error 500s
+    # Test that the web hooks handle connection errors and error 500s
+    # test handle 500 error codes
+    # test handlr 503 error code
+    @patch("hc.api.transports.requests.request")
+    def test_webhook_handles_500_error(self, mock_post):
+        self._setup_data("webhook", "http://example")
+        mock_post.return_value.status_code = 503
+
+        self.channel.notify(self.check)
+        n = Notification.objects.get()
+        self.assertEqual(n.error, "Received status code 503")
