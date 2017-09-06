cfe8509eb79c814a6b9428e71568ddaeb164d2db
dhywty <clemwek@gmail.com>
2017-09-06 10:12:15 +0300

    Fixed string to bytes

--- hc/api/tests/test_create_check.py
+++ hc/api/tests/test_create_check.py
@@ -18,7 +18,7 @@ class CreateCheckTestCase(BaseTestCase):
         if expected_error:
             self.assertEqual(r.status_code, 400)
             ### Assert that the expected error is the response error
-            self.assertEqual(json.loads(r.content)['error'], expected_error)
+            self.assertEqual(json.loads(force_text(r.content))['error'], expected_error)
 
         return r
 
@@ -68,7 +68,7 @@ class CreateCheckTestCase(BaseTestCase):
         r = {'status_code': 400, 'error': "could not parse request body"} ### This is just a placeholder variable
         r = self.client.post(self.URL, ["this is my data"], content_type='application/json')
         self.assertEqual(r.status_code, 400)
-        self.assertEqual(json.loads(r.content)['error'], "could not parse request body")
+        self.assertEqual(json.loads(force_text(r.content))['error'], "could not parse request body")
 
     def test_it_rejects_wrong_api_key(self):
         self.post({"api_key": "wrong"},
