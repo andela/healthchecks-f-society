1f59a5497de9c1a4d4c731fd53a2195a78f0b5af
cjmash <collomash4@gmail.com>
2017-09-06 11:26:21 +0300

    [feature #150704022] work on admin tests

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -5,15 +5,17 @@ from hc.test import BaseTestCase
 class ApiAdminTestCase(BaseTestCase):
 
     def setUp(self):
+
         super(ApiAdminTestCase, self).setUp()
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
-
+        self.alice.is_staff = True
+        self.alice.is_superuser = True
+        self.alice.save()
         ### Set Alice to be staff and superuser and save her :)
 
     def test_it_shows_channel_list_with_pushbullet(self):
         self.client.login(username="alice@example.org", password="password")
-
         ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
         ch.save()
-
+        self.assertEqual(ch.kind ,"pushbullet")
         ### Assert for the push bullet
