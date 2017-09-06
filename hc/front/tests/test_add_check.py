e6f3cbe93a3879e0c37b871fec03f5eba452700d
Lewis Mbogo <ogomb>
2017-09-06 11:14:29 +0300

    [Feature #150704024] add test for checking team access works

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -2,6 +2,8 @@ from hc.api.models import Check
 from hc.test import BaseTestCase
 
 
+
+
 class AddCheckTestCase(BaseTestCase):
 
     def test_it_works(self):
@@ -12,3 +14,15 @@ class AddCheckTestCase(BaseTestCase):
         assert Check.objects.count() == 1
 
     ### Test that team access works
+    def test_team_access_works(self):
+        url = "/checks/add/"
+        
+        self.client.login(username="charlie@example.org", password="password")
+        r = self.client.post(url)
+     
+        check = Check.objects.get()
+        self.assertNotEqual(check.user, self.alice)
+
+
+
+  
