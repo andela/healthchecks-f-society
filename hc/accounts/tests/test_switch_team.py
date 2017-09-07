50327f28ad9ca60db428d401a60692616b2679d8
chadwalt <chadwalt@outlook.com>
2017-09-07 16:07:32 +0300

    [Feature #150704023] Add switch_team tests

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -14,14 +14,18 @@ class SwitchTeamTestCase(BaseTestCase):
         r = self.client.get(url, follow=True)
 
         ### Assert the contents of r
-
+        self.assertRedirects(r, "/checks/")
+        
 
     def test_it_checks_team_membership(self):
         self.client.login(username="charlie@example.org", password="password")
 
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url)
+
         ### Assert the expected error code
+        self.assertEqual(r.status_code, 403)
+        
 
     def test_it_switches_to_own_team(self):
         self.client.login(username="alice@example.org", password="password")
@@ -29,3 +33,5 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url, follow=True)
         ### Assert the expected error code
+        self.assertRedirects(r, "/checks/")
+        
