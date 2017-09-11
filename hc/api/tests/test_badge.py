eeb2a644536aaea57af6d2b34b220bfed5dff936
cjmash <collomash4@gmail.com>
2017-09-11 11:25:53 +0300

    [Chore #150704022] Refactor use pep8 naming conventions

--- hc/api/tests/test_badge.py
+++ hc/api/tests/test_badge.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.core.signing import base64_hmac
-
 from hc.api.models import Check
 from hc.test import BaseTestCase
 
@@ -13,14 +12,14 @@ class BadgeTestCase(BaseTestCase):
 
     def test_it_rejects_bad_signature(self):
         """Test it regects bad signature"""
-        r = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
-        result = r.status_code
-        self.assertEquals(result, 400)
+        result = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
+        response = result.status_code
+        self.assertEquals(response, 400)
 
     def test_it_returns_svg(self):
         """Test it returns a svg"""
         sig = base64_hmac(str(self.alice.username), "foo", settings.SECRET_KEY)
         sig = sig[:8].decode("utf-8")
         url = "/badge/%s/%s/foo.svg" % (self.alice.username, sig)
-        r = self.client.get(url)
-        self.assertContains(r, "xml")
+        result = self.client.get(url)
+        self.assertContains(result, "xml")
