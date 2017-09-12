7523d73e12e2ca1a0bab5e14c61ebb806062dd5e
Lewis Mbogo <ogomb>
2017-09-12 16:37:36 +0300

    [Feature #150704027] Update file to remove add checks button

---
+++ hc/front/tests/test_failed_checks.py
@@ -0,0 +1,27 @@
+from hc.api.models import Check
+from hc.test import BaseTestCase
+from datetime import timedelta as td
+from django.utils import timezone
+
+
+class MyChecksTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(MyChecksTestCase, self).setUp()
+        self.check = Check(user=self.alice)
+        self.check.save()
+
+    def test_it_shows_red_check(self):
+        self.check.last_ping = timezone.now() - td(days=3)
+        self.check.status = "up"
+        self.check.save()
+
+        self.client.login(username="alice@example.org", password="password")
+        response = self.client.get("/failed_checks/")
+
+        # Desktop
+        self.assertContains(response, "icon-down")
+
+        # Mobile
+        self.assertContains(response, "label-danger")
+        
