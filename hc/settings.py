5680af661960f555717fda1fe22c58fe773c5b81
Collin Mutembei <mutembei.collin@gmail.com>
2017-09-19 14:04:21 +0300

    Reintroduce Heroku changes
    Squashed commit of the following:
    
    commit f042bbf2b304d60fe72a27f9ee75865b4af8cd21
    Author: brian kabiro <briankigondu@gmail.com>
    Date:   Mon Sep 18 14:59:50 2017 +0300
    
        [Chore #151000651] Add whitenoise middleware
    
        I included the whitenoise middleware in the middleware section of the settings file
    
    commit d414c62c07b7a0f68301003e8cbfd03f34182203
    Author: devGenie <jonen54@gmail.com>
    Date:   Fri Sep 15 13:04:39 2017 +0300
    
        [CHORE 151000651] FIX genenv typo
    
    commit 2155f8162a3633a01e8c5c8b7a3011e874c06950
    Author: devGenie <jonen54@gmail.com>
    Date:   Fri Sep 15 13:02:37 2017 +0300
    
        [CHORE 151000651] Add dynamic site root
    
    commit cb4b319ff45ad3c702f92737d3aca2d5e117919f
    Author: devGenie <jonen54@gmail.com>
    Date:   Fri Sep 15 12:32:42 2017 +0300
    
        [CHORE 151000651] Add connection max age
    
    commit b5c1c859a0479a5a65ed35849c9f2d70bddda131
    Author: devGenie <jonen54@gmail.com>
    Date:   Fri Sep 15 12:28:26 2017 +0300
    
        [CHORE 151000651] Update with dj_database_url dependency
    
    commit d88443d6e91095a893027a87ed1d0e4d0fecc7c4
    Author: devGenie <jonen54@gmail.com>
    Date:   Fri Sep 15 12:23:19 2017 +0300
    
        [CHORE 151000651] Add heroku procfile
    
    commit d3d08112df2414a0732c8bc9ee57f29ed99fd809
    Author: devGenie <jonen54@gmail.com>
    Date:   Fri Sep 15 12:22:40 2017 +0300
    
        [CHORE 151000651] Add db settings

--- hc/settings.py
+++ hc/settings.py
@@ -21,6 +21,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 DEFAULT_FROM_EMAIL = 'healthchecks@example.org'
 USE_PAYMENTS = False
+SITE_ROOT = "http://localhost:8000"
 
 
 INSTALLED_APPS = (
@@ -49,6 +50,7 @@ MIDDLEWARE = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'hc.accounts.middleware.TeamAccessMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 )
 
 AUTHENTICATION_BACKENDS = (
@@ -84,7 +86,7 @@ TEST_RUNNER = 'hc.api.tests.CustomRunner'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':   './hc.sqlite',
+        'NAME': './hc.sqlite',
     }
 }
 
@@ -93,9 +95,9 @@ DATABASES = {
 if os.environ.get("DB") == "postgres":
     DATABASES = {
         'default': {
-            'ENGINE':   'django.db.backends.postgresql',
-            'NAME':     'hc',
-            'USER':     'postgres',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'hc',
+            'USER': 'postgres',
             'TEST': {'CHARSET': 'UTF8'}
         }
     }
@@ -104,10 +106,18 @@ if os.environ.get("DB") == "mysql":
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'USER':     'root',
-            'NAME':     'hc',
+            'USER': 'root',
+            'NAME': 'hc',
             'TEST': {'CHARSET': 'UTF8'}
         }
+    }
+
+if os.getcwd() == "/app":
+    SITE_ROOT = os.getenv("SITE_ROOT")
+    from dj_database_url import parse
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASES = {
+        "default": parse(DATABASE_URL, conn_max_age=600)
     }
 
 LANGUAGE_CODE = 'en-us'
@@ -120,7 +130,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-SITE_ROOT = "http://localhost:8000"
 PING_ENDPOINT = SITE_ROOT + "/ping/"
 PING_EMAIL_DOMAIN = HOST
 STATIC_URL = '/static/'
